9f5411e5397582286b8fb95852defdaace144247
othmaneslaimani2-jpg <othmaneslaimani2@gmail.com>
2026-06-25 15:02:55 +0100

    add GET method for the api

--- server.js
+++ server.js
@@ -17,24 +17,46 @@ const main = async () => {
             id SERIAL PRIMARY KEY,
             name VARCHAR(50) NOT NULL,
             author_name VARCHAR(100),
+            category VARCHAR(100),
             is_available BOOLEAN DEFAULT true,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         );`;
+        
         await db.query(createTableBooks)
+        app.get("/", (req, res) => {
+            res.json({
+                message: "Bienvenue d'ans l'API Bibliotech"
+            })
+        });
+        app.get("/livres", async (req, res) => {
+            try {
+                const result = await pool.query("SELECT * FROM livres");
+                res.status(200).json(result.rows);
+            
+            } catch (error) {
+                res.status(500).json({
+                    message: "Server Error",
+                    error: error.message
+                });
+            }
+        })
         app.post("/livres", async (req, res) => {
             try {
-                const { name, isAvailable, authorName } = req.body
-                const insertQuery = `INSERT INTO livres (name, author_name, is_available)
-                VALUES ($1,$2,$3);`
-                await db.query(insertQuery, [name, authorName, isAvailable])
-                res.status(201).json({ message: "The Book has been created" })
+                const { name, authorName, category, isAvailable, created_at } = req.body
+                const insertQuery = `
+INSERT INTO livres
+(name, author_name, category, is_available, created_at)
+VALUES ($1, $2, $3, $4, $5)
+`;
+                await db.query(insertQuery, [name, authorName, category, isAvailable, created_at])
+                res.status(201).json({ message: "The Books has been created" })
             } catch (_err) {
                 console.log(_err)
                 res.status(403).json({ error: 'Error Occured please provide all information needed' })
             }
         })
-        
-        app.listen(5020, () => console.log("This app is listning on port 5020"))
+
+        app.listen(5433, () => console.log("This app is listning on port 5433"))
     } catch (error) {
         console.log("Oups something went wrong!")
     }
